fe5e66764daea230393eb706cbdf43909f359ae9
Joelkons02 <118935303+Joelkons02@users.noreply.github.com>
2023-03-15 00:42:20 +0100

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,74 +1,37 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
-/**
-  * is_palindrome - Checks if a singly linked list is a palindrome
-  * @head: The head of the singly linked list
-  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
-  */
-int is_palindrome(listint_t **head)
+listint_t *is_pal_recurs(listint_t *left, listint_t *right)
 {
-    listint_t *start = NULL, *end = NULL;
-    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
-
-    if (head == NULL)
-        return (0);
-
-    if (*head == NULL)
-        return (1);
-    
-    start = *head;
-    len = listint_len(start);
-    len_cyc = len * 2;
-    len_list = len_cyc - 2;
-    end = *head;
-    for (; i < len_cyc; i = i + 2)
-    {
-        if (start[i].n != end[len_list].n)
-            return (0);
-        len_list = len_list - 2;
-    }
-    return (1);
-}
-
-/**
-  * get_nodeint_at_index - Gets a node from a linked list
-  * @head: The head of the linked list
-  * @index: The index to find in the linked list
-  * Return: The specific node of the linked list
-  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	listint_t *current = head;
-	unsigned int iter_times = 0;
-
-	if (head)
+	if (right->next == NULL)
+		return (left->next);
+	right = right->next;
+	if (right->next == NULL)
 	{
-		while (current != NULL)
-		{
-			if (iter_times == index)
-				return (current);
-			current = current->next;
-			++iter_times;
-		}
+		if (left->n != left->next->n)
+			return (NULL);
+		if (left->next->next == NULL)
+			return (left);
+		return (left->next->next);
+	}
+	right = is_pal_recurs(left->next, right->next);
+	if (right == NULL)
+		return (NULL);
+	if (right->n == left->n)
+	{
+		if (right->next == NULL)
+			return (left);
+		return (right->next);
 	}
 	return (NULL);
 }
+		
 
-/**
-  * slistint_len - Counts the number of elements in a linked list
-  * @h: The linked list to count
-  * Return: Number of elements in the linked list
-  */
-size_t listint_len(const listint_t *h)
+int is_palindrome(listint_t **head)
 {
-	int lenght = 0;
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (1);
 
-	while (h != NULL)
-	{
-		++lenght;
-		h = h->next;
-	}
-	return (lenght);
+	if (is_pal_recurs(*head, *head) != NULL)
+		return (1);
+	return (0);
 }

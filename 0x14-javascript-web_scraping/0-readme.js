7f3b2c8ac12d6597eddce907dfdba3c090cae177
Joelkons02 <konsenj02@gmail.com>
2023-07-26 00:46:26 +0000

    init

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filename = process.argv[2];
+
+try {
+  const content = fs.readFileSync(filename, 'utf-8');
+  console.log(content);
+} catch (error) {
+  console.error(error);
+}

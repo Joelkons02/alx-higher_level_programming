549f296b7e9dbf2125684a8f89977ffcb6a09b03
Joelkons02 <konsenj02@gmail.com>
2023-07-26 01:07:01 +0000

    init

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const url = process.argv[2];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const todos = JSON.parse(body);
-    const completedTasks = todos.filter(todo => todo.completed);
-    const completedTasksByUserId = {};
-    completedTasks.forEach(todo => {
-      const userId = todo.userId;
-      if (!completedTasksByUserId[userId]) {
-        completedTasksByUserId[userId] = 0;
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
-      completedTasksByUserId[userId]++;
-    });
-    console.log(completedTasksByUserId);
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
